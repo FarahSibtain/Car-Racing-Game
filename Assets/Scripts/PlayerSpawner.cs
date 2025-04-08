f088cbbcc13ae9e3d4fd9d2ea99ce63832b9cb7d
FarahXR <ATBB9vY5WVvxX6es8uDTgbTx4AVc4D6DC0A2>
2025-04-08 15:13:37 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,24 @@
+﻿using Cinemachine;
+using Fusion;
+using UnityEngine;
+
+public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
+{
+    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] CinemachineVirtualCamera freeLookCamera;
+    public void PlayerJoined(PlayerRef player)
+    {
+        if (player == Runner.LocalPlayer)
+        {
+            NetworkObject playerObj = Runner.Spawn(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+            SetCamera(playerObj.transform);
+        }
+    }
+
+    private void SetCamera(Transform playerTransform)
+    {
+        freeLookCamera.m_Follow = playerTransform;
+        freeLookCamera.m_LookAt = playerTransform;
+    }
+}
