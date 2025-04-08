f4fb0fb80e0ef290cfbd88c0cf0a1832c27b2a14
FarahXR <ATBB9vY5WVvxX6es8uDTgbTx4AVc4D6DC0A2>
2025-04-08 15:32:32 +0200

    more changes

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -7,13 +7,40 @@ public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
     [SerializeField] private GameObject playerPrefab;
     [SerializeField] private Transform spawnPoint;
     [SerializeField] CinemachineVirtualCamera freeLookCamera;
+    [SerializeField] int horizontalGap;
+    [SerializeField] int verticalGap;
+    int playerCount = 0;
+    //[Networked, Capacity(12)] private NetworkDictionary<PlayerRef, NetworkObject> Players => default;
     public void PlayerJoined(PlayerRef player)
     {
         if (player == Runner.LocalPlayer)
         {
-            NetworkObject playerObj = Runner.Spawn(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+            NetworkObject playerObj = Runner.Spawn(playerPrefab, GetCarPosition(), spawnPoint.rotation);            
+            //Players.Add(player, playerObj);
             SetCamera(playerObj.transform);
+            playerCount++;
         }
+    }
+    Vector3 GetCarPosition()
+    {
+        //determine the position of the car 
+        float posX = 0;
+        float posZ = 0;
+        switch (playerCount % 3)
+        {
+            case 0:
+                posZ = spawnPoint.transform.position.z;
+                break;
+            case 1:
+                posZ = spawnPoint.transform.position.z + horizontalGap;
+                break;
+            case 2:
+                posZ = spawnPoint.transform.position.z + 2 * horizontalGap;
+                break;
+        }
+        posX = spawnPoint.transform.position.x + (playerCount / 3) * verticalGap;
+
+        return new Vector3(posX, spawnPoint.transform.position.y, posZ);
     }
 
     private void SetCamera(Transform playerTransform)
